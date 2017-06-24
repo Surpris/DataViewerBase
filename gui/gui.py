d837d32a94fd11d072751f512deb496066e86d9e
Surpris <take90-it09-easy27@outlook.jp>
2017-06-25 00:13:40 +0900

    add log messages describing what function works now.

--- gui/gui.py
+++ gui/gui.py
@@ -6,6 +6,7 @@ Created on Thu Jun 22 23:22:08 2017
 """
 
 import sys
+import inspect
 import os
 import json
 import datetime
@@ -24,51 +25,64 @@ class DataViewerBase(QMainWindow):
         Initialization.
         """
         super().__init__()
+        print("Initialize this application...")
         self.initInnerParameters()
         self.initGui()
     
     def initInnerParameters(self):
-        self._currentDir = os.path.dirname(__file__)
-        self._online = False
-        self._closing_dialog = True
-        if os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
-            with open(os.path.join(os.path.dirname(__file__), "config.json"),'r') as ff:
-                config = json.load(ff)
-            if config.get("currentDir") is not None:
-                if isinstance(config.get("currentDir"), str):
-                    if os.path.exists(config.get("currentDir")):
-                        self._currentDir = config["currentDir"]
-            if config.get("online") is not None:
-                if isinstance(config.get("online"), bool):
-                    self._online = config["online"]
-            if config.get("closing_dialog") is not None:
-                if isinstance(config.get("closing_dialog"), bool):
-                    self._closing_dialog = config["closing_dialog"]
-        print(self._currentDir)
+        """
+        Initialize the inner parameters.
+        """
+        print(">>" + inspect.currentframe().f_code.co_name)
+        try:
+            self._currentDir = os.path.dirname(__file__)
+            self._online = False
+            self._closing_dialog = True
+            if os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
+                with open(os.path.join(os.path.dirname(__file__), "config.json"),'r') as ff:
+                    config = json.load(ff)
+                if config.get("currentDir") is not None:
+                    if isinstance(config.get("currentDir"), str):
+                        if os.path.exists(config.get("currentDir")):
+                            self._currentDir = config["currentDir"]
+                if config.get("online") is not None:
+                    if isinstance(config.get("online"), bool):
+                        self._online = config["online"]
+                if config.get("closing_dialog") is not None:
+                    if isinstance(config.get("closing_dialog"), bool):
+                        self._closing_dialog = config["closing_dialog"]
+        except Exception as ex:
+            print(ex)
+        print("<<" + inspect.currentframe().f_code.co_name)
     
     def initGui(self):
         """
         Initialize the GUI.
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.initMainWidget()
         self.setMenuBar()
 
         self.setWindowTitle("VMI Viewer")
         self.resize(1200, 600)
+        print("<<" + inspect.currentframe().f_code.co_name)
     
     def initMainWidget(self):
         """
-            Initialize the main widget and the grid
+        Initialize the main widget and the grid.
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         self.main_widget = QWidget(self)
         self.grid = QGridLayout(self.main_widget)
         self.grid.setSpacing(10)
+        print("<<" + inspect.currentframe().f_code.co_name)
 
     def setMenuBar(self):
         """
         Set the contents of the menu bar
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         ## File
         file_menu = QMenu('&File', self)
 
@@ -86,12 +100,15 @@ class DataViewerBase(QMainWindow):
         help_menu.addAction('About...', self.showAbout)
         self.menuBar().addSeparator()
         self.menuBar().addMenu(help_menu)
+        print("<<" + inspect.currentframe().f_code.co_name)
     
     def openFile(self):
         """
         Show a file dialog and select a file
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         pass
+        print("<<" + inspect.currentframe().f_code.co_name)
 
     def quitApp(self):
         """
@@ -100,6 +117,7 @@ class DataViewerBase(QMainWindow):
         self.close()
     
     def closeEvent(self, event):
+        print(">>" + inspect.currentframe().f_code.co_name)
         if self._closing_dialog:
             confirmObject = QMessageBox.question(self, "Closing...",
                 "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No,
@@ -115,22 +133,33 @@ class DataViewerBase(QMainWindow):
             config = self.makeConfig()
             with open(os.path.join(os.path.dirname(__file__), "config.json"), "w") as ff:
                 json.dump(config, ff)
+        print("<<" + inspect.currentframe().f_code.co_name)
     
     def makeConfig(self):
-        return {"online":self._online, "closing_dialog":self._closing_dialog, 
+        """
+        Make a config dict object to save the latest configration in.
+        """
+        print(">>" + inspect.currentframe().f_code.co_name)
+        config = {"online":self._online, "closing_dialog":self._closing_dialog, 
                 "currentDir":self._currentDir}
+        print("<<" + inspect.currentframe().f_code.co_name)
+        return config
     
     def showHelp(self):
         """
         Show a pop-up dialog showing how to use this application.
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         pass
+        print("<<" + inspect.currentframe().f_code.co_name)
     
     def showAbout(self):
         """
         Show a pop-up dialog describing this application.
         """
+        print(">>" + inspect.currentframe().f_code.co_name)
         pass
+        print("<<" + inspect.currentframe().f_code.co_name)
 
 def main():
     app = QtGui.QApplication([])
