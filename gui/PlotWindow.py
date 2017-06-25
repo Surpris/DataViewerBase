e7625d4c740509c6480caf24bdaa4361bb016f84
Surpris <take90-it09-easy27@outlook.jp>
2017-06-25 14:21:23 +0900

    test plotting a random-noise image, its x/y projections and its histogram.

--- gui/PlotWindow.py
+++ gui/PlotWindow.py
@@ -31,34 +31,36 @@ class PlotWindow(QDialog):
         grid = QGridLayout(self)
         grid.setSpacing(10)
 
-        ### Some buttons.
+        ## Some buttons.
         button_test = QPushButton()
         button_test.setText("Test")
         QObject.connect(button_test, SIGNAL("clicked()"), self.pushButton)
         grid.addWidget(button_test, 0, 0, 1, 1)
         
+        ## Construct the graphic layout.
         self.glw = pg.GraphicsLayoutWidget()
         self.glw.resize(800, 800)
-        
-        # self.grid = QGridLayout(self.w)
-        # self.grid.setSpacing(10)
 
-        self.pw2 = pg.PlotItem()
-        self.iw = pg.ImageItem()
-
-        self.p1 = self.glw.addPlot()
-        self.p1.setMaximumWidth(200)
+        # Plot area for the image.
         p1 = self.glw.addPlot()
+        self.iw = pg.ImageItem()
         p1.addItem(self.iw)
+
+        # Plot area for x-projection.
+        self.px = self.glw.addPlot()
+        self.px.setMaximumWidth(250)
+
         self.glw.nextRow()
-        self.p2 = self.glw.addPlot(col=1)
-        self.p2.setMaximumHeight(200)
-        try:
-            _x = np.arange(0, 100) * 0.1*np.pi
-            self.p2.plot(_x, np.sin(_x))
-        except Exception as ex:
-            print(ex)
-        self.iw.setImage(np.random.randint(0, 100, (100, 100)))
+
+        # Plot area for y-projection.
+        self.py = self.glw.addPlot()
+        self.py.setMaximumHeight(250)
+
+        # Plot area for histogram.
+        self.ph = self.glw.addPlot()
+        self.ph.setMaximumHeight(250)
+        self.ph.setMaximumWidth(250)
+
         grid.addWidget(self.glw, 1, 0)
         
         self.data = None
@@ -67,4 +69,13 @@ class PlotWindow(QDialog):
         self.exec_()
     
     def pushButton(self):
-        pass
+        self.updateImage()
+
+    def updateImage(self):
+        data = np.random.randint(0, 100, (100, 100))
+        self.iw.setImage(data)
+        self.px.plot(data.sum(axis=1), clear=True)
+        self.py.plot(data.sum(axis=0), clear=True)
+        hist, xbins = np.histogram(data.flatten())
+        dxbins = xbins[1] - xbins[0]
+        self.ph.plot(xbins[:-1] + dxbins/2.0, hist, clear=True)
